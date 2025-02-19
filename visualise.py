91e01589370908f7bafe9e3ea3ed64256b776745
108000207 <timothy@gapp.nthu.edu.tw>
2025-02-19 22:00:14 +0800

    [update] visualise need update norm_stats npz path [debug] show dim less then given default num

--- visualise.py
+++ visualise.py
@@ -12,8 +12,10 @@ def visualise_gan(data_continuous_real, data_continuous_syn, data_discrete_real,
 
     # renorm
     DATA_PATH = "data/real/mimic/"
-    min_val_con = np.load(os.path.join(DATA_PATH, "norm_stats.npz"))["min_val"]
-    max_val_con = np.load(os.path.join(DATA_PATH, "norm_stats.npz"))["max_val"]
+    filename_postfix = '5_var'
+
+    min_val_con = np.load(os.path.join(DATA_PATH, f"norm_stats_{filename_postfix}_mimiciv.npz"))["min_val"]
+    max_val_con = np.load(os.path.join(DATA_PATH, f"norm_stats_{filename_postfix}_mimiciv.npz"))["max_val"]
     data_continuous_real = renormlizer(data_continuous_real, max_val_con, min_val_con)
     data_continuous_syn = renormlizer(data_continuous_syn, max_val_con, min_val_con)
 
@@ -24,8 +26,15 @@ def visualise_gan(data_continuous_real, data_continuous_syn, data_discrete_real,
     # Set the ticks and ticklabels for all axes
     plt.setp(axes, xticks=[0, 3, 6, 9, 12, 15, 18, 21, 24])
 
-    # randomly select [num_plot, num_dim] features from real data and visualise
-    c_dim_list  = random.sample(list(range(data_continuous_real.shape[2])), num_dim)
+    # Ensure num_dim is not greater than the population size and not negative
+    population_size = data_continuous_real.shape[2]
+    if num_dim > population_size:
+        num_dim = population_size
+    if num_dim < 0:
+        num_dim = 0
+    # randomly select [num_plot, num_dim] features from real data and visualise 
+    c_dim_list  = random.sample(list(range(population_size)), num_dim)
+    # c_dim_list  = random.sample(list(range(data_continuous_real.shape[2])), num_dim)
     c_pid_index = random.sample(list(range(len(data_continuous_real))), num_plot)
     
     for i in range(num_dim):
@@ -39,8 +48,16 @@ def visualise_gan(data_continuous_real, data_continuous_syn, data_discrete_real,
         df = pd.DataFrame(data_continuous_syn[c_pid_index, :, c_dim_list[i]])
         sns.lineplot(ax=axes[1, i], data=df.T, palette=sns.color_palette('Reds', n_colors=num_plot))
 
+    # Ensure num_dim is not greater than the population size and not negative
+    population_size = data_discrete_real.shape[2]
+    if num_dim > population_size:
+        num_dim = population_size
+    if num_dim < 0:
+        num_dim = 0
+
+    d_dim_list = random.sample(list(range(population_size)), num_dim)
     # randomly select [num_plot, num_dim] features from real data and visualise
-    d_dim_list = random.sample(list(range(data_discrete_real.shape[2])), num_dim)
+    # d_dim_list = random.sample(list(range(data_discrete_real.shape[2])), num_dim)
     d_pid_index = random.sample(list(range(len(data_discrete_real))), num_plot)
 
     for i in range(num_dim):
@@ -60,8 +77,9 @@ def visualise_vae(data_continuous_real, data_continuous_syn, data_discrete_real,
 
     # renorm
     DATA_PATH = "data/real/mimic/"
-    min_val_con = np.load(os.path.join(DATA_PATH, "norm_stats.npz"))["min_val"]
-    max_val_con = np.load(os.path.join(DATA_PATH, "norm_stats.npz"))["max_val"]
+    filename_postfix = '5_var'
+    min_val_con = np.load(os.path.join(DATA_PATH, f"norm_stats_{filename_postfix}_mimiciv.npz"))["min_val"]
+    max_val_con = np.load(os.path.join(DATA_PATH, f"norm_stats_{filename_postfix}_mimiciv.npz"))["max_val"]
     data_continuous_real = renormlizer(data_continuous_real, max_val_con, min_val_con)
     data_continuous_syn = renormlizer(data_continuous_syn, max_val_con, min_val_con)
 
@@ -72,7 +90,14 @@ def visualise_vae(data_continuous_real, data_continuous_syn, data_discrete_real,
     # Set the ticks and ticklabels for all axes
     plt.setp(axes, xticks=[0, 3, 6, 9, 12, 15, 18, 21, 24])
 
-    c_dim_list  = random.sample(list(range(data_continuous_real.shape[2])), num_dim)
+    # Ensure num_dim is not greater than the population size and not negative
+    population_size = data_continuous_real.shape[2]
+    if num_dim > population_size:
+        num_dim = population_size
+    if num_dim < 0:
+        num_dim = 0
+    c_dim_list  = random.sample(list(range(population_size)), num_dim)
+    # c_dim_list  = random.sample(list(range(data_continuous_real.shape[2])), num_dim)
     c_pid_index = random.sample(list(range(len(data_continuous_syn))), num_plot)
     
     for i in range(len(c_dim_list)):
@@ -83,7 +108,15 @@ def visualise_vae(data_continuous_real, data_continuous_syn, data_discrete_real,
         df = pd.DataFrame(data_continuous_syn[c_pid_index, :, c_dim_list[i]])
         sns.lineplot(ax=axes[0, i], data=df.T, marker='o', palette=sns.color_palette('Reds', n_colors=num_plot))
 
-    d_dim_list = random.sample(list(range(data_discrete_real.shape[2])), num_dim) 
+    population_size = data_discrete_real.shape[2]
+    if num_dim > population_size:
+        num_dim = population_size
+    if num_dim < 0:
+        num_dim = 0
+
+    d_dim_list = random.sample(list(range(population_size)), num_dim)
+
+    # d_dim_list = random.sample(list(range(data_discrete_real.shape[2])), num_dim) 
     d_pid_index = random.sample(list(range(len(data_discrete_syn))), num_plot)
 
     for i in range(len(d_dim_list)):
